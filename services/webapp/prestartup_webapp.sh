ec4eec10714e8612a41f4becde1c029ec62292bd
Stefano Alberto Russo <stefano.russo@gmail.com>
2021-11-04 18:08:15 +0100

    Fixed permissions issue.

--- services/webapp/prestartup_webapp.sh
+++ services/webapp/prestartup_webapp.sh
@@ -4,7 +4,10 @@ set -e
 # Set proper permissions to the log dir
 chown rosetta:rosetta /var/log/webapp
 
-# Create and set proper permissions to the data/resources
+# Create and set proper permissions to the data/resources and shared dir
 mkdir -p /data/resources 
 chown rosetta:rosetta /data/resources
+chown rosetta:rosetta /shared
+
+
 
